c2e38b1dcb3f5717463212fc6f4b60954c65f94f
tomg404 <32769321+tomg404@users.noreply.github.com>
2022-08-31 01:49:17 +0200

    change on environment variables

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-### SET ZSH CONFIG DIR TO BE IN .config
-export ZDOTDIR='$HOME/.config/zsh'
-source /etc/profile
+### SETUP ENVIRONMENT VARIABLES
+touch $HOME/.zshenv
+echo 'export XDG_CONFIG_HOME="$HOME/.config"' > $HOME/.zshenv
+echo 'export ZDOTDIR="$XDG_CONFIG_HOME/zsh"' > $HOME/.zshenv
+
+### SYMLINK ZSH-CONFIG to .config/zsh
+stow zsh
 
 ### INSTALL LATEST ZSH VERSION
 sudo apt-get install zsh -y
@@ -11,4 +15,5 @@ sudo apt-get install zsh -y
 sudo chsh -s $(which zsh)
 
 ### INSTALL OHMYZSH
-ZSH="$HOME/.config/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#ZSH="$HOME/.config/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
