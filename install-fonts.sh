b4151084ac21c1b2c1809f1627b891a2b3e710ae
tomg404 <32769321+tomg404@users.noreply.github.com>
2022-08-31 19:37:00 +0200

    modifications to font installation script

--- install-fonts.sh
+++ install-fonts.sh
@@ -3,14 +3,25 @@
 # This script is adapted from:
 # https://github.com/mu-gaimann/mise-en-place
 
+url="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip"
+file="Meslo.zip"
+
 ### Download Meslo font 
-curl https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+if curl -o "${PWD}/${file}" -L ${url}; then
+    echo 'Font downloaded successfully'
+else
+    echo 'Something went wrong while downloading'
+    exit 1
+fi
 
 ### Unzip font and install to local font directory
 mkdir -p ~/.local/share/fonts/meslo-nerd-fonts/
-unzip -o Meslo.zip -d ~/.local/share/fonts/meslo-nerd-fonts/
-rm Meslo.zip
+unzip -o ${file} -d ~/.local/share/fonts/meslo-nerd-fonts/
+rm ${file}
 
 ### Rebuild font cache
 fc-cache ~/.local/share/fonts
 fc-cache -f -v
+
+### Output
+echo 'Font was installed successfully!'
