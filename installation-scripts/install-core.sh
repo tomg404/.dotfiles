b2639562b1842c4b4938385aae2f88c120deadc6
tomg404 <accnts.tom@gmail.com>
2022-11-27 21:34:12 +0100

    added 2 packages and commented some packages

--- installation-scripts/install-core.sh
+++ installation-scripts/install-core.sh
@@ -7,21 +7,23 @@ sudo apt-get upgrade -y
 ### CORE STUFF
 sudo apt-get install git -y
 sudo apt-get install curl -y
-sudo apt-get install stow -y
+sudo apt-get install stow -y            # managing config files
+sudo apt-get install okular -y          # best pdf reader
 
 ### BASIC TERMINAL STUFF
 sudo apt-get install tmux -y
-sudo apt-get install neofetch -y
-sudo apt-get install neovim -y
+sudo apt-get install neovim -y          # maybe remove: need version >= 0.8.0 for astronvim
 
 ### SYSTEM STUFF
 sudo apt-get install lm-sensors -y
-sudo apt-get install tlp -y
+sudo apt-get install tlp -y             # maybe remove
 
 ### BACKUP AND SECURITY
-sudo apt-get install keepassxc -y
+sudo apt-get install keepassxc -y       # best password manager
 sudo apt-get install synology-drive -y
 
 ### FILE PROCESSING
 sudo apt-get install unzip -y
 
+### LIBRARIES
+sudo apt-get install libfuse2 -y        # for appimages
