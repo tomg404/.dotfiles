5a035ea0721d764415e2d97af0fd29b779bfa7a0
tomg404 <32769321+tomg404@users.noreply.github.com>
2023-03-20 16:18:44 +0100

    small change

--- astronvim/.config/nvim/lua/user/init.lua
+++ astronvim/.config/nvim/lua/user/init.lua
@@ -232,7 +232,7 @@ local config = {
       ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
       ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
       -- quick save
-      -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+      -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command      
     },
     t = {
       -- setting a mapping to false will disable it
