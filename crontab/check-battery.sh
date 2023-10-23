3e2a2cdff337b08f5c131e9a4941369ea956626b
Tom Gaimann <tom.gaimann@tum.de>
2023-10-23 13:54:42 +0200

    tweaked battery checking script

--- crontab/check-battery.sh
+++ crontab/check-battery.sh
@@ -1,35 +1,48 @@
 #!/bin/bash
-# stole script from: https://www.reddit.com/r/i3wm/comments/g9qtig/comment/foxd397
+# adapted script from: https://www.reddit.com/r/i3wm/comments/g9qtig/comment/foxd397
 
+# necessary for dunst
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
-# low and critical battery level.
-# at low battery level, notification will be sent to inform the user.
-# at critcal battery level, urgent notification will be sent.
-# range = 0 - 100
-low=15
+# settings. low / critical battery thresholds (range: 0-100)
+low=80
 critical=5
+icon_path="/usr/share/icons/Papirus/48x48/status/battery-low.svg"
 
-# get battery-level(0-100%) and state(discharging/charging)
+# check for tmp file existence
+tmp_file_path="/tmp/check-battery-script-notified"
+if [ ! -f "$tmp_file_path" ] ; then
+  echo "Creating tmp file..."
+  echo "0" > "$tmp_file_path"
+fi
+
+# get battery level(0-100%), state(discharging/charging) and if user was already notified
 battery=$(cat /sys/class/power_supply/BAT0/capacity)
 state=$(cat /sys/class/power_supply/BAT0/status)
-notified=$(cat /tmp/check-battery-script-notified)
-echo $battery
-echo $state
-echo $notified
+notified=$(cat "$tmp_file_path")
+echo "Battery percentage: $battery%"
+echo "Battery state: $state"
+echo "Already notified: $notified"
 
+# notification logic
 if [ "$state" = "Discharging" ] ; then
-    # if battery discharging and below low level then send notification
-    if [ $battery -gt $critical ] && [ $battery -le $low ] && [ $notified -eq 0 ] ; then
-        notify-send -a "Battery" "Low" "Plugin to Recharge" --icon=battery-low
-        let notified=1
-    # if battery discharging and below critical level then send notification and wait for user action
-    elif [ $battery -le $critical ] ; then
-        notify-send -a "Battery" "Critically low" "Backup Data or Plugin to Recharge" --icon=battery-low --urgency=critical --wait
-    fi
+    
+  # if battery discharging and below low level then send notification
+  if [ $battery -gt $critical ] && [ $battery -le $low ] && [ $notified -eq 0 ] ; then
+    notify-send -a "Battery" "Low" "Plugin to Recharge" --icon="$icon_path"
+    let notified=1
+
+  # if battery discharging and below critical level then send notification and wait for user action
+  elif [ $battery -le $critical ] ; then
+    notify-send -a "Battery" "Critically low" "Backup Data or Plugin to Recharge" --icon="$icon_path" --urgency=critical --wait
+    
+  fi
+
 else
-    let notified=0
+    
+  let notified=0
+
 fi
-echo $notified > /tmp/check-battery-script-notified
+echo "$notified" > "$tmp_file_path"
 
 exit 0
