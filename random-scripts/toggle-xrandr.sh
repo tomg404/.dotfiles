5d7f22a0990ca90c5e4f74f842ec0add2f777e5e
Tom Gaimann <tom.gaimann@tum.de>
2024-12-07 12:32:30 +0100

    fix screenlayout toggle

--- random-scripts/toggle-xrandr.sh
+++ random-scripts/toggle-xrandr.sh
@@ -5,8 +5,9 @@ HDMI_STATUS=$(cat /sys/class/drm/*-HDMI-*/status)
 if [ "$HDMI_STATUS" = "connected" ]; then
 	echo "execute dock.sh"
 	sh "$HOME/.screenlayout/dock.sh"
-	i3-msg 'restart' >/dev/null
 else
 	echo "execute standard.sh"
 	sh "$HOME/.screenlayout/standard.sh"
 fi
+sleep 2
+i3-msg 'restart' >/dev/null
