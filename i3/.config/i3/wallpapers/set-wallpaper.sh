af969dc55896a675f97fe261326b53cb32e08d48
Tom Gaimann <tom.gaimann@tum.de>
2025-01-22 15:20:42 +0100

    load base64 encoded wallpaper

---
+++ i3/.config/i3/wallpapers/set-wallpaper.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <file-name>"
+  echo "The file has to be base64 encoded"
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+  echo "Error: Could not read file $1"
+  exit 1
+fi
+
+WALLPAPER_B64="$1"
+WALLPAPER=$(mktemp)
+
+base64 -d "$WALLPAPER_B64" > "$WALLPAPER" 2> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Error: Could not decode file $1"
+  exit 1
+fi
+
+feh --no-fehbg --bg-scale "$WALLPAPER"
