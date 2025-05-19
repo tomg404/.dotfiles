528ee504e9a930766000a9997e2d75f18a608981
Tom Gaimann <tom.gaimann@tum.de>
2025-05-19 11:58:50 +0200

    send notification when finished

--- scripts/clean-downloads.sh
+++ scripts/clean-downloads.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
 # TODO: maybe switch to deletion by `creation date` (-ctime) as this
 # would delete files after x days regardless of their modifcation date.
 # this would be a way stricter policy and would keep the download
@@ -15,9 +17,13 @@ DAY_THRESHOLD=7
 # delete files
 # N=$(find "$DOWNLOADS" -mtime "+$DAY_THRESHOLD" -type f -printf '.' | wc -c)
 N=$(find "$DOWNLOADS" -mtime "+$DAY_THRESHOLD" -type f -printf '.' -exec rm {} \; | wc -c)
-echo "Deleted $N files"
 
 # delete empty (top level) folders
 # N=$(find "$DOWNLOADS" -maxdepth 1 -type d -empty -printf '.' | wc -c)
-N=$(find "$DOWNLOADS" -maxdepth 1 -type d -empty -printf '.' -delete | wc -c)
-echo "Deleted $N empty folders"
+M=$(find "$DOWNLOADS" -maxdepth 1 -type d -empty -printf '.' -delete | wc -c)
+
+echo "Deleted $N files in $M folders from $DOWNLOADS"
+notify-send \
+  -a 'Downloads Cleaner' \
+  -i '/usr/share/icons/Papirus-Dark/24x24/actions/trash-empty.svg' \
+  "Deleted $N files in $M folders from $DOWNLOADS"
