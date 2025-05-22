77691ac1d4470a95dfef62bcc0a4cc4fe70c95a7
Tom Gaimann <tom.gaimann@tum.de>
2025-05-23 00:16:36 +0200

    fix arrow key rebind in insert mode

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,5 +1,7 @@
+local opts = { noremap = true, silent = true }
+
 -- set the leader key to <Space>
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', opts)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -8,19 +10,16 @@ vim.keymap.set('i', 'jj', '<ESC>', {})
 
 -- shortcuts for telescope
 local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
+vim.keymap.set('n', '<leader>ff', telescope.find_files, opts)
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, opts)
 
 -- disable arrow keys in every mode
-vim.keymap.set('n', '<Up>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('n', '<Down>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('n', '<Left>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('n', '<Right>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('i', '<Up>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('i', '<Down>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('i', '<Left>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('i', '<Right>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('v', '<Up>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('v', '<Down>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('v', '<Left>', ":echoe 'Stop using arrow keys!'<CR>")
-vim.keymap.set('v', '<Right>', ":echoe 'Stop using arrow keys!'<CR>")
+vim.keymap.set({'n', 'v'}, '<Up>',    ":echoe 'Stop using arrow keys!'<CR>", opts)
+vim.keymap.set({'n', 'v'}, '<Down>',  ":echoe 'Stop using arrow keys!'<CR>", opts)
+vim.keymap.set({'n', 'v'}, '<Left>',  ":echoe 'Stop using arrow keys!'<CR>", opts)
+vim.keymap.set({'n', 'v'}, '<Right>', ":echoe 'Stop using arrow keys!'<CR>", opts)
+vim.keymap.set('i', '<Up>',    "<ESC> :echoe 'Stop using arrow keys!<CR>", opts)
+vim.keymap.set('i', '<Down>',  "<ESC> :echoe 'Stop using arrow keys!<CR>", opts)
+vim.keymap.set('i', '<Left>',  "<ESC> :echoe 'Stop using arrow keys!<CR>", opts)
+vim.keymap.set('i', '<Right>', "<ESC> :echoe 'Stop using arrow keys!<CR>", opts)
+
